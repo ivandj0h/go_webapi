27e40cfa4854729e3e1203b73a2cd1ac68681e95
ivandi1980 <ivandi.gah@luxpmsoft.com>
2021-09-19 00:43:01 +0800

    create routes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,17 +10,74 @@ import (
 func main() {
 	router := gin.Default()
 
-	// The 1st Route
-	router.GET("/", func(c *gin.Context) {
+	// The 1st Route (GET)
+	router.GET("/", rootHandler)
+	router.GET("/hello", helloHandler)
+	router.GET("/user/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
 		c.JSON(http.StatusOK, gin.H{
-			"email":            "someone1@example.com",
-			"password":         "mys3cr3T",
-			"firstName":        "Firstname",
-			"lastName":         "Lastname",
-			"title":            "Mr",
-			"primaryTelephone": "442071234567",
+			"id": id,
 		})
 	})
+	router.GET("user/query", queryHandler)
 
+	// The 2nd Route (POST)
+	router.POST("/users", postUserHandler)
+
+	// run the program
 	router.Run()
+}
+
+// create rootHandler
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"email":            "juna@ivandjoh.online",
+		"password":         "mys3cr3T",
+		"firstName":        "Juna",
+		"lastName":         "Djoh",
+		"title":            "Mr",
+		"primaryTelephone": "442071234567",
+	})
+}
+
+// create helloHandler
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"title": "Hello World!",
+	})
+}
+
+// Create queryHandler
+func queryHandler(c *gin.Context) {
+	nama := c.Query("nama")
+
+	c.JSON(http.StatusOK, gin.H{
+		"nama": nama,
+	})
+}
+
+// create Struct
+type UserInput struct {
+	username string
+	email    string
+	age      int
+	address  string
+}
+
+// Create postUserHandler
+func postUserHandler(c *gin.Context) {
+	var userInput UserInput
+
+	err := c.ShouldBindJSON(&userInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"username": userInput.username,
+		"email":    userInput.email,
+		"age":      userInput.age,
+		"address":  userInput.address,
+	})
 }
