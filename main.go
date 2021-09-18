2bf489f8144685df879ba2bb8ef5b108f68a8499
ivandi1980 <ivandi.gah@luxpmsoft.com>
2021-09-18 20:29:48 +0800

    create the first route

--- main.go
+++ main.go
@@ -9,10 +9,15 @@ import (
 func main() {
 	router := gin.Default()
 
+	// The 1st Route
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"Name": "Juna",
-			"age":  "5",
+			"email":            "someone1@example.com",
+			"password":         "mys3cr3T",
+			"firstName":        "Firstname",
+			"lastName":         "Lastname",
+			"title":            "Mr",
+			"primaryTelephone": "442071234567",
 		})
 	})
 
